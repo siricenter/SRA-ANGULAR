2e66166a16a88994c0259ab3099970a8b6e01b86
CJPoll <CJPoll@gmail.com>
2015-01-02 08:56:16 -0700

    Fixed error in Gruntfile

--- Gruntfile.js
+++ Gruntfile.js
@@ -369,7 +369,7 @@ module.exports = function (grunt) {
 				'watch'
 		]);
 	});
-	grunt.registerTask('server', 'DEPRECATED TASK. Use the 'serve' task instead', function (target) {
+	grunt.registerTask('server', 'DEPRECATED TASK. Use the \'serve\' task instead', function (target) {
 		grunt.log.warn('The `server` task has been deprecated. Use `grunt serve` to start a server.');
 		grunt.task.run(['serve:' + target]);
 	});
