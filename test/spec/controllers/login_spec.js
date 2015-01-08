fb32265bde18c84ac9558c0be7c3696faf31f2c8
CJPoll <CJPoll@gmail.com>
2015-01-08 14:54:07 -0700

    Tests are finally up and running! YAY!!!!!!

---
+++ test/spec/controllers/login_spec.js
@@ -0,0 +1,28 @@
+'use strict';
+
+describe( 'Controller: LoginController', function () {
+
+	// load the controller's module
+	beforeEach( function () {
+		module( 'sraAngularApp' );
+	});
+
+	var subject, scope, setup;
+
+	setup = function ( $controller, $rootScope ) {
+		scope = $rootScope.$new();
+		subject = $controller( 'LoginController', {
+			$scope: scope
+		});
+	};
+
+	// Initialize the controller and a mock scope
+	beforeEach( 
+			inject( setup ));
+
+	it( 'has the login function', function () {
+		expect( scope ).not.toBeNull();
+		expect( scope.Login ).not.toBeNull();
+	});
+
+});
