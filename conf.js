fa0d22bf43c8365e8077032e5e072d8249e93290
Cody Poll <CJPoll@gmail.com>
2015-02-14 06:59:25 -0700

    Browser restarts between each test until we figure out how to logut.

--- conf.js
+++ conf.js
@@ -1,6 +1,7 @@
 exports.config = {
 	seleniumAddress: 'http://localhost:4444/wd/hub',
 	specs: ['protractor/**/*.js'],
+	restartBrowserBetweenTests: true,
 
 	capabilities: {
 		'browserName': 'firefox'
